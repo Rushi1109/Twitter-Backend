05a6af3f6a8ca1cae14261f90e9fb2758a89635e
Rushi_Gandhi <rushigandhi1109@gmail.com>
2023-08-18 18:11:45 +0530

    Add tweetId to already present tags in database

--- src/repository/hashtag-repository.js
+++ src/repository/hashtag-repository.js
@@ -46,7 +46,7 @@ class HashtagRepository {
         try {
             const tags = await Hashtag.find({
                 title: titleList,
-            }).select('title -_id');
+            });
             return tags;
         } catch (error) {
             console.log(error);
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -12,21 +12,25 @@ class TweetService {
 
         // RegEx for extracting hashtags
         const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1));
-        console.log(tags);
 
         // Calling create method of tweet-repository
         const tweet = await this.tweetRepository.create(data);
 
         if (tags) {
             let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
-            alreadyPresentTags = alreadyPresentTags.map((tag) => tag.title);
-            let newTags = tags.filter((tag) => !alreadyPresentTags.include(tag));
+            let titleOfPresentTags = alreadyPresentTags.map((tag) => tag.title);
+            let newTags = tags.filter((tag) => !titleOfPresentTags.includes(tag));
 
             newTags = newTags.map((tag) => {
                 return { title: tag, tweets: [tweet.id] }
             });
 
             await this.hashtagRepository.bulkCreate(newTags);
+
+            alreadyPresentTags.forEach((tag) => {
+                tag.tweets.push(tweet.id);
+                tag.save();
+            });
         }
 
         return tweet;
