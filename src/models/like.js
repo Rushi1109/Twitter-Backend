8312be8882064fcf880857d4fa63f88ebeb73da7
Rushi_Gandhi <rushigandhi1109@gmail.com>
2023-08-19 15:27:42 +0530

    Created v1 model of likeSchema and userSchema

---
+++ src/models/like.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema({
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet', 'Comment'],
+    },
+    likeable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel',
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    }
+},
+    {
+        timestamps: true,
+    });
+
+const Like = mongoose.model('Like', likeSchema);
+
+export default Like;
---
+++ src/models/user.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+},
+    {
+        timestamps: true,
+    });
+
+const User = mongoose.Model('User', userSchema);
+
+export default User;
